8ac0a43fd579594d55dc2daf124bb841fddd6e33
Tri <DELL@TRIPC>
2022-11-07 00:28:55 +0700

    Commit to Driver

--- BusManagement/src/main/java/com/busmanagement/Driver.java
+++ BusManagement/src/main/java/com/busmanagement/Driver.java
@@ -29,13 +29,6 @@ abstract class Person {
     String getBankName() {
         return bankName;
     }
-    //get info
-    public void getInfo() {
-    	System.out.println("Name: " + name);
-    	System.out.println("Telephone Number: " + tel);
-    	System.out.println("Bank Number: " + bankNumber);
-    	System.out.println("Bank Name: " + bankName);
-    }
 }
 
 // class Driver inherit abstract class Person
@@ -105,16 +98,6 @@ class Driver extends Person{
     	this.shift = shift;
     }
     
-  //override view info from Person
-    @Override
-    public void getInfo() {
-    	System.out.println("Id: " + id);
-    	System.out.println("Driver: " + name);
-    	System.out.println("Telephone Number: " + tel);
-    	System.out.println("Bank Number: " + bankNumber);
-    	System.out.println("Bank Name: " + bankName);
-    	System.out.println("Shift: " + shift);
-    }
 }
 //add getID and getShift if needed
 class Manager extends Person {
