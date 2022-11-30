1be7671a0583f20f6ae4103450d96cd52727693f
VIP <VIP@DESKTOP-KH1S2U8>
2022-11-30 20:50:16 +0700

    Update code to match with report and change (temporarily)

--- BusManagement/src/main/java/com/busmanagement/Main.java
+++ BusManagement/src/main/java/com/busmanagement/Main.java
@@ -3,33 +3,26 @@ package com.busmanagement;
 public class Main {
 
 	public static void main(String args[]) {
-	        Driver tom = new Driver("Tom", "0914257933", "97146022", "VCB", "18148","Day");
+	        Driver tom = new Driver("18148", "Tom", "0914257933","Day", "Unavailable");
 	        /*System.out.println("Driver: " + tom.getName());
 	        System.out.println("Telephone Number: " + tom.getTel());*/
 	        tom.getInfo();
-			/*
-			 * tom.duty(true); System.out.println("Message from "+tom.getName()+
-			 * ": "+tom.communicate("Clear")+" "); System.out.println(tom.checkIn());
-			 */
-	        Route A14 = new Route(14,"VGU","Nga tu Binh Phuoc","Ben xe Mien Dong","Hang Xanh","Ho con Rua");
+	        //Route A14 = new Route(14,"VGU","Nga tu Binh Phuoc","Ben xe Mien Dong","Hang Xanh","Ho con Rua");
+	        Route A14 = new Route(14, 50, 8000, 10, "Hi");
 	        A14.getInfo();
-	        Bus A2 = new Bus(tom, A14, "61B 61773", "Ho con Rua", 30, 40, 8000, 50);
+	        Bus A2 = new Bus(tom, A14, "61B 61773", 30, 40, "Unavailable");
 	        A2.getInfo();
-	        A2.run(true);
 	        
-	        //Driver jerry = new Driver("Jerry", "0918025543", "17355","Afternoon");
-	        Driver jerry = new Driver("Jerry", "0918025543", "92831251", "VCB", "17355","Afternoon");
+	        Driver jerry = new Driver("17355", "Jerry", "0918025543","Afternoon", "Avaiable");
 	        /*System.out.println("Driver: " + jerry.getName());
 	        System.out.println("Telephone Number: " + jerry.getTel());*/
 	        jerry.getInfo();
-			/*
-			 * jerry.duty(false); jerry.checkOut();
-			 */
-	        Route A51 = new Route(51,"VGU","Hikari","Thu Vien Tinh Binh Duong","Nga Ba Lo Chen","Ben xe Mien Dong");
+	        //Route A51 = new Route(51,"VGU","Hikari","Thu Vien Tinh Binh Duong","Nga Ba Lo Chen","Ben xe Mien Dong");
+	        Route A51 = new Route(51, 0, 8000, 8, "Hello");
 	        A51.getInfo();
-	        Bus A1 = new Bus(jerry, A51,"61B 61554", "Hikari", 50, 0, 8000, 0);
+	        Bus A1 = new Bus(jerry, A51,"61B 61554", 50, 0, "Available");
 	        A1.getInfo();
-	        A1.run(false);
+
 	        
 	}
 
--- BusManagement/src/main/java/com/busmanagement/Route.java
+++ BusManagement/src/main/java/com/busmanagement/Route.java
@@ -1,25 +1,47 @@
 
 package com.busmanagement;
 
-
 public class Route {
-    public int routeNumber;
-    public String[] stop= new String[5];
+	public int routeNumber;
+	public int ticketPrice;
+	public int totalDistance;
+	public String description;
+	private int ticketCount;
 
-    public Route (int routeNumber,String stop1,String stop2,String stop3,String stop4,String stop5) {
-        this.routeNumber = routeNumber;
-        this.stop[0] = stop1;
-        this.stop[1] = stop2;
-        this.stop[2] = stop3;
-        this.stop[3] = stop4;
-        this.stop[4] = stop5;
-    }
-    public void getInfo() {
-        
-        System.out.println("\nRoute Number: "+ routeNumber);
-        for (int i = 0; i < 5; i++){
-        System.out.println("Bus Stop " + (i+1) + " : "+stop[i]);
-    }
-        System.out.print("\n");
-    }
+	/*
+	 * public String[] stop= new String[5];
+	 * 
+	 * 
+	 * public Route (int routeNumber,String stop1,String stop2,String stop3,String
+	 * stop4,String stop5) { this.routeNumber = routeNumber; this.stop[0] = stop1;
+	 * this.stop[1] = stop2; this.stop[2] = stop3; this.stop[3] = stop4;
+	 * this.stop[4] = stop5; }
+	 */
+	public Route(int routeNumber, int ticketCount, int ticketPrice, int totalDistance, String description) {
+		this.routeNumber = routeNumber;
+		this.ticketCount = ticketCount;
+		this.ticketPrice = ticketPrice;
+		this.totalDistance = totalDistance;
+		this.description = description;
+	}
+
+	public int getTicketCount() {
+		return ticketCount;
+	}
+	public void setTicketCount(int ticketCount) {
+		this.ticketCount = ticketCount;
+	}
+	public void getInfo() {
+
+		System.out.println("Route Number: " + routeNumber);
+		System.out.println("Ticket Count: " + ticketCount);
+		System.out.println("Ticket Price: " + ticketPrice + "vnd");
+		System.out.println("Total Distance: " + totalDistance + "km");
+		System.out.println("Description: " + description);
+		/*
+		 * for (int i = 0; i < 5; i++) { System.out.println("Bus Stop " + (i + 1) +
+		 * " : " + stop[i]); }
+		 */
+		System.out.println();
+	}
 }
