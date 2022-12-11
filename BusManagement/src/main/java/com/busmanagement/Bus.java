bc631a6f9428c4fcfb0cbbbe0bf657387035dc18
VIP <VIP@DESKTOP-KH1S2U8>
2022-12-11 09:37:06 +0700

    Update "fuelCapacity attributes"

--- BusManagement/src/main/java/com/busmanagement/Bus.java
+++ BusManagement/src/main/java/com/busmanagement/Bus.java
@@ -9,18 +9,18 @@ public class Bus implements Vehicle {
     public int seat;
     int id;
     double remainingFuel;
-    double maxFuelCapacity;
+    double fuelCapacity;
     String status;
     Driver driver;
     Route route;
   
-    public Bus (Driver driver, Route route, int id, String plateNumber, double remainingFuel, double maxFuelCapacity, int seat, String status) {
+    public Bus (Driver driver, Route route, int id, String plateNumber, double remainingFuel, double fuelCapacity, int seat, String status) {
         this.driver = driver;
         this.route = route;
         this.id = id;
         this.plateNumber = plateNumber;
         this.remainingFuel = remainingFuel;
-        this.maxFuelCapacity = maxFuelCapacity;
+        this.fuelCapacity = fuelCapacity;
         this.seat = seat;
         this.status = status;
     }
@@ -29,7 +29,7 @@ public class Bus implements Vehicle {
     	System.out.println("Id Number: " + id);
         System.out.println("Plate Numbers: " + plateNumber); 
         System.out.println("Remaining Fuel: " +  remainingFuel);
-        System.out.println("Max Fuel Capacity: " + maxFuelCapacity);
+        System.out.println("Max Fuel Capacity: " + fuelCapacity);
         System.out.println("Seat: " + seat);
         System.out.println("Status: " + status);
         System.out.println();
